3890ce68ebf8321c13fb898e061ee0768ef9f3ca
Sergio Martins <sergiomos2010@gmail.com>
2021-10-26 19:44:23 -0300

    Feat: refact repos response

--- src/helpers/getOldestRepos.js
+++ src/helpers/getOldestRepos.js
@@ -1,18 +1,15 @@
 const axios = require('axios');
 
-const serialize = (repo) => {
-  const { created_at, ...rest } = repo
+const serialize = ({ name, owner, description, language }) => ({ 
+  name, 
+  coverImageUrl: owner.avatar_url, 
+  description,
+  language,
+});
 
-  return {
-    createdAt: created_at,
-    ...rest,
-  }
-}
+const parseToObject = (acc, repo, index) => ({ ...acc, [index]: repo, });
 
-const parseToObject = (acc, repo, index) => ({...acc, [index]: repo, });
-
-
-const getOldestRepos = async() => {
+const getOldestRepos = async () => {
   const { data } = await axios.get('https://api.github.com/orgs/takenet/repos');
   const repositories = data.filter(({ language }) => language === 'C#')
     .slice(0, 5)
